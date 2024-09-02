6b9f89b0e15e7fea8c84edec48c8f89ba6a85c1c
podeig <denis@digits.no>
2024-09-03 00:08:49 +0200

    Refactoring

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,6 @@
 
 import Card from "@/components/Card";
 import { Button, Spinner } from "@radix-ui/themes";
-import { format } from "date-fns";
-import Link from "next/link";
 import { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import Search from "@/components/Search";
@@ -21,11 +19,7 @@ export default function Page() {
   const [inputQuery, setInputQuery] = useState<string>("");
   const [domain, setDomain] = useState<string>("techcrunch.com");
 
-  const fetchArticles = async (
-    page: number,
-    searchQuery: string,
-    domain: string
-  ) => {
+  const fetchArticles = async () => {
     try {
       setLoading(true);
       const response = await fetch(
@@ -46,27 +40,19 @@ export default function Page() {
   };
 
   useEffect(() => {
-    fetchArticles(page, searchQuery, domain);
+    fetchArticles();
   }, [page, searchQuery, domain]);
 
-  const onNavigate = (url: string) => {
-    console.log("URL:", url);
-  };
-
-  if (error) {
-    return <div>{error.toString()}</div>;
-  }
-
-  const resetSearch = () => {
+  const resetPage = () => {
     setArticles([]);
     setPage(1);
     setNoMoreArticles(false);
   };
 
-  const onChange = (query: string) => {
+  const onChangeSearch = (query: string) => {
     clearTimeout(timer.current);
     timer.current = setTimeout(() => {
-      resetSearch();
+      resetPage();
       setSearchQuery(query);
     }, 500);
     setInputQuery(query);
@@ -74,7 +60,7 @@ export default function Page() {
 
   const onChangeDomain = (domain: string) => {
     setDomain(domain);
-    resetSearch();
+    resetPage();
   };
 
   return (
@@ -90,7 +76,7 @@ export default function Page() {
         <div className="flex justify-center mt-4">
           <Search
             query={inputQuery}
-            onChange={(query) => onChange(query)}
+            onChange={(query) => onChangeSearch(query)}
             className="mr-1"
           />
           <SelectDomain
@@ -102,37 +88,12 @@ export default function Page() {
         </div>
       </div>
       <div className="flex gap-0 flex-row flex-wrap w-full py-4">
-        {articles?.map((article: Article, key: number) => (
-          <Link
-            key={key}
-            href={article.url}
-            scroll={false}
-            className="lg:w-1/3 w-full relative bg-transparent top-0 hover:bg-yellow-50 hover:-top-[15px] transition-all duration-200 bg-opacity-50 hover:scale-103 cursor-pointer"
-            target="_black"
-            type="button"
-          >
-            <Card className="border-gray-200 p-6">
-              <div
-                className="flex flex-col h-full"
-                onClick={() => onNavigate(article.url)}
-              >
-                {article.urlToImage && (
-                  <img
-                    src={article.urlToImage}
-                    alt={article.title}
-                    width={400}
-                    className="mx-auto image-old oldImage border-[20px] border-white"
-                  />
-                )}
-                <time className="px-2 text-sm mt-4 font-bold">
-                  {format(article.publishedAt, "dd.MM.yyyy HH:MM")}
-                </time>
-                <h2 className="lg:text-xl p-2 font-semibold">
-                  {article.title}
-                </h2>
-              </div>
-            </Card>
-          </Link>
+        {articles?.map((article: Article) => (
+          <Card
+            article={article}
+            key={article.url}
+            className="border-gray-200 p-6"
+          />
         ))}
         <div className="flex justify-center p-10 w-full">
           {loading && <Spinner size="3" />}
@@ -148,6 +109,10 @@ export default function Page() {
             >
               More articles
             </Button>
+          )}
+
+          {error && (
+            <div className="text-red-300 p-10">An error occurred: {error}</div>
           )}
         </div>
       </div>
--- components/Card.tsx
+++ components/Card.tsx
@@ -1,8 +1,36 @@
+import { format } from "date-fns";
+import Link from "next/link";
+
 type Props = {
-  children: React.ReactNode;
+  article: Article;
   className: string;
 };
 
-export default function Card({ children, className }: Props) {
-  return <div className={`${className}`}>{children}</div>;
+export default function Card({ article, className }: Props) {
+  return (
+    <Link
+      href={article.url}
+      scroll={false}
+      className="lg:w-1/3 w-full relative bg-transparent top-0 hover:bg-yellow-50 hover:-top-[15px] transition-all duration-200 bg-opacity-50 hover:scale-103 cursor-pointer"
+      target="_black"
+      type="button"
+    >
+      <div className={`${className}`}>
+        <div className="flex flex-col h-full">
+          {article.urlToImage && (
+            <img
+              src={article.urlToImage}
+              alt={article.title}
+              width={400}
+              className="mx-auto image-old oldImage border-[20px] border-white"
+            />
+          )}
+          <time className="px-2 text-sm mt-4 font-bold">
+            {format(article.publishedAt, "dd.MM.yyyy HH:MM")}
+          </time>
+          <h2 className="lg:text-xl p-2 font-semibold">{article.title}</h2>
+        </div>
+      </div>
+    </Link>
+  );
 }
